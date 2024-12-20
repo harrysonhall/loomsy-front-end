e999f8429f4f3c717069f9bd4132b77be1afb134
harrysonhall <harryson.hall@gmail.com>
2024-12-20 16:23:30 -0500

    cleanup

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      
+    </div>
+  );
 };
 
 export default Home;
